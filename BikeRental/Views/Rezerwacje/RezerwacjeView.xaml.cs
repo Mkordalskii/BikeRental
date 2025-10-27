326c75835fec2970cc9f04308c90edc07955389c
Maciej <m.kordalski0@gmail.com>
2025-10-27 18:12:35 +0100

    Add project files.

---
+++ BikeRental/Views/Rezerwacje/RezerwacjeView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BikeRental.Views.Rezerwacje
+{
+    /// <summary>
+    /// Interaction logic for RezerwacjeView.xaml
+    /// </summary>
+    public partial class RezerwacjeView : UserControl
+    {
+        public RezerwacjeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BikeRental/Views/Wypozyczenia/AktywneWypozyczeniaView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BikeRental.Views.Wypozyczenia
+{
+    /// <summary>
+    /// Interaction logic for AktywneWypozyczeniaView.xaml
+    /// </summary>
+    public partial class AktywneWypozyczeniaView : UserControl
+    {
+        public AktywneWypozyczeniaView()
+        {
+            InitializeComponent();
+        }
+    }
+}
