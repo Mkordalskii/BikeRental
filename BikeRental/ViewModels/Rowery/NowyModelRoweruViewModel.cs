d0355513e30242aee6956814bfa65ad20a393178
Maciej <m.kordalski0@gmail.com>
2025-11-10 18:05:15 +0100

    update Views and ViewModels

--- BikeRental/ViewModels/Rowery/NowyModelRoweruViewModel.cs
+++ BikeRental/ViewModels/Rowery/NowyModelRoweruViewModel.cs
@@ -11,7 +11,96 @@ namespace BikeRental.ViewModels
         {
             DisplayName = "Dodaj/edytuj modele";
             item = new RowerModel();
+            E_Bike = false;
         }
+        #endregion
+        #region Properties
+        public string Producent
+        {
+            get
+            {
+                return item.Producent;
+            }
+            set
+            {
+                if (item.Producent != value)
+                {
+                    item.Producent = value;
+                    OnPropertyChanged(() => Producent);
+                }
+            }
+        }
+        public string Nazwa
+        {
+            get
+            {
+                return item.Nazwa;
+            }
+            set
+            {
+                if (item.Nazwa != value)
+                {
+                    item.Nazwa = value;
+                    OnPropertyChanged(() => Nazwa);
+                }
+            }
+        }
+        public string Typ
+        {
+            get
+            {
+                return item.Typ;
+            }
+            set
+            {
+                if (item.Typ != value)
+                {
+                    item.Typ = value;
+                    OnPropertyChanged(() => Typ);
+                }
+            }
+        }
+        public bool E_Bike
+        {
+            get
+            {
+                return item.E_Bike;
+            }
+            set
+            {
+                if (item.E_Bike != value)
+                {
+                    item.E_Bike = value;
+                    OnPropertyChanged(() => E_Bike);
+                    OnPropertyChanged(() => NotE_Bike);
+                }
+            }
+        }
+        public bool NotE_Bike
+        {
+            get => !E_Bike;
+            set
+            {
+                //ustawiamy Sprawny jako negację value
+                E_Bike = !value;
+            }
+        }
+        public decimal? MasaKg
+        {
+            get
+            {
+                return item.MasaKg;
+            }
+            set
+            {
+                if (item.MasaKg != value)
+                {
+                    item.MasaKg = value;
+                    OnPropertyChanged(() => MasaKg);
+                }
+            }
+        }
+
         #endregion
         #region Commands
 
--- BikeRental/ViewModels/Stojaki/NowyStojakViewModel.cs
+++ BikeRental/ViewModels/Stojaki/NowyStojakViewModel.cs
@@ -11,7 +11,66 @@ namespace BikeRental.ViewModels
         {
             DisplayName = "Dodaj stojak";
             item = new Stojak();
+            Sprawny = true;
         }
+        #endregion
+        #region Properties
+        public Stacja Stacja
+        {
+            get
+            {
+                return item.Stacja;
+            }
+            set
+            {
+                if (item.Stacja != value)
+                {
+                    item.Stacja = value;
+                    OnPropertyChanged(() => Stacja);
+                }
+            }
+        }
+        public int NumerMiejsca
+        {
+            get
+            {
+                return item.NumerMiejsca;
+            }
+            set
+            {
+                if (item.NumerMiejsca != value)
+                {
+                    item.NumerMiejsca = value;
+                    OnPropertyChanged(() => NumerMiejsca);
+                }
+            }
+        }
+        public bool Sprawny
+        {
+            get
+            {
+                return item.Sprawny;
+            }
+            set
+            {
+                if (item.Sprawny != value)
+                {
+                    item.Sprawny = value;
+                    OnPropertyChanged(() => Sprawny);
+                    OnPropertyChanged(() => Niesprawny);
+                }
+            }
+        }
+        public bool Niesprawny
+        {
+            get => !Sprawny;
+            set
+            {
+                //ustawiamy Sprawny jako negację value
+                Sprawny = !value;
+            }
+        }
+
         #endregion
         #region Commands
         public override void Save()
