4b1e790c61c115120a376242121c7ed04e6ca7e7
Maciej <m.kordalski0@gmail.com>
2025-12-21 19:40:13 +0100

    stage

---
+++ BikeRental/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BikeRental.ViewModels
+{
+    public class HomeViewModel : WorkspaceViewModel
+    {
+        public HomeViewModel()
+        {
+            this.DisplayName = "Strona główna";
+        }
+    }
+}
