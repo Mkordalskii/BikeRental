9dfaece414cb1b3e8cffe9172f168ff9a54d61d0
Maciej <m.kordalski0@gmail.com>
2026-01-21 17:45:51 +0100

    add Bussiness logic to the HomePage

---
+++ BikeRental/Models/BusinessLogic/HomePageB.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace BikeRental.Models.BusinessLogic
+{
+    public class HomePageB : DatabaseClass
+    {
+        public HomePageB(BikeRentalDbEntities db) : base(db)
+        {
+        }
+        #region Funkcje pomocnicze
+        public int GetActiveRentalsCount()
+        {
+            return db.Wypozyczenie
+                .Where(wypozyczenie => wypozyczenie.CzyAktywny == true)
+                .Count();
+        }
+        public int GetAvilableBikesCount()
+        {
+            return db.Rower
+                .Where(rower => rower.CzyAktywny == true && rower.SlownikRowerStan.Nazwa == "Dostępny")
+                .Count();
+        }
+        public int GetReservedBikesCount()
+        {
+            return db.Rezerwacja
+                .Where(rezerwacja => rezerwacja.CzyAktywny == true)
+                .Count();
+        }
+        public decimal GetTotalRevenue()
+        {
+            return db.WypozyczenieOplata
+                .Where(oplata => oplata.CzyAktywny == true)
+                .Sum(oplata => oplata.Kwota);
+        }
+        #endregion
+    }
+}
--- BikeRental/ViewModels/HomeViewModel.cs
+++ BikeRental/ViewModels/HomeViewModel.cs
@@ -1,10 +1,80 @@
-﻿namespace BikeRental.ViewModels
+﻿using BikeRental.Models;
+using BikeRental.Models.BusinessLogic;
+
+namespace BikeRental.ViewModels
 {
     public class HomeViewModel : WorkspaceViewModel
     {
+        #region Baza danych
+        private readonly BikeRentalDbEntities db;
+        #endregion
+        #region Konstruktor
         public HomeViewModel()
         {
+            db = new BikeRentalDbEntities();
             this.DisplayName = "Strona główna";
+            _homePageB = new HomePageB(db);
+            AktywneWypozyczenia = _homePageB.GetActiveRentalsCount();
+            DostepneRowery = _homePageB.GetAvilableBikesCount();
+            ZarezerwowaneRowery = _homePageB.GetReservedBikesCount();
+            LacznePrzychody = _homePageB.GetTotalRevenue();
         }
+        #endregion
+        #region Pola i wlasciwosci
+        private HomePageB _homePageB;
+        private int _AktywneWypozyczenia;
+        private int _DostepneRowery;
+        private int _ZarezerwowaneRowery;
+        private decimal _LacznePrzychody;
+        public int AktywneWypozyczenia
+        {
+            get
+            {
+                return _AktywneWypozyczenia;
+            }
+            set
+            {
+                _AktywneWypozyczenia = value;
+                OnPropertyChanged(() => AktywneWypozyczenia);
+            }
+        }
+        public int DostepneRowery
+        {
+            get
+            {
+                return _DostepneRowery;
+            }
+            set
+            {
+                _DostepneRowery = value;
+                OnPropertyChanged(() => DostepneRowery);
+            }
+        }
+        public int ZarezerwowaneRowery
+        {
+            get
+            {
+                return _ZarezerwowaneRowery;
+            }
+            set
+            {
+                _ZarezerwowaneRowery = value;
+                OnPropertyChanged(() => ZarezerwowaneRowery);
+            }
+        }
+        public decimal LacznePrzychody
+        {
+            get
+            {
+                return _LacznePrzychody;
+            }
+            set
+            {
+                _LacznePrzychody = value;
+                OnPropertyChanged(() => LacznePrzychody);
+            }
+        }
+        #endregion
+
     }
 }
